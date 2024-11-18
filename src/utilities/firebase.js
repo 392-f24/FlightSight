9a7c09bb723766463f0fc28ee125a5bec53829f0
angelp03 <angelpimentel2025@u.northwestern.edu>
2024-11-17 22:18:47 -0600

    Initialize firebase app and hosting

---
+++ src/utilities/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBt2VGm8meEPfSl9J7UT0wtXb96vFX0Pqk",
+  authDomain: "flightsight-538c5.firebaseapp.com",
+  projectId: "flightsight-538c5",
+  storageBucket: "flightsight-538c5.firebasestorage.app",
+  messagingSenderId: "732082271722",
+  appId: "1:732082271722:web:fbfc1eb868e248e840ee93"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
